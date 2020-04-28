d1b11c32fc11399ef25919510685f9e14f9402d7
chenshuguang1995 <911728942@qq.com>
2020-04-28 18:53:44 +0800

    简单重构

--- fisher.py
+++ fisher.py
@@ -1,11 +1,15 @@
-from flask import Flask
-from helper import is_isbn_or_key
+import json
 
+from flask import Flask, jsonify
+from helper import is_isbn_or_key
+from yushu_book import YuShuBook
 
 app = Flask(__name__)
+print('id为' + str(id(app)) + '的app实例化')
 app.config.from_object('config')
+
 # 这种方式导入flask默认必须大写
-print(app.config['DEBUG'])
+# print(app.config['DEBUG'])
 
 
 @app.route('/hello')
@@ -13,16 +17,10 @@ def hello():
     return 'Hello Chen'
 
 
-@app.route('/book/search/<q>/<page>')
-def search(q, page):
-    """
-        q普通关键字 isbn  isbn13:13位数字   isbn10:0到9,中间包含'-'
-        page
-    """
-    is_isbn_or_key(q)
-
+from app.web import book
 
 
 if __name__ == '__main__':
+    print('id为' + str(id(app)) + '的app启动')
     # 生产环境 Nginx+uwsgi部署,不加会产生两个web服务器
     app.run(host='0.0.0.0', debug=app.config['DEBUG'])
